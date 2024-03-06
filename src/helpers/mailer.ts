48a72599dca4e9aa77650aede577ec60e24fd607
bhanu-sh <bhanu1234sharma@gmail.com>
2024-03-06 15:37:23 +0530

    mailroutes

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -28,8 +28,8 @@ export const sendEmail = async ({email, emailType, userId} : any) => {
             from: 'bhanu@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "Verify your email." : "Reset your password."}
-            or copy and paste the link below in your browser: <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            html: `<p>Click <a href="${process.env.DOMAIN}/${emailType === "VERIFY" ? "verifyemail" : "verifyforgetpasswordtoken"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "Verify your email." : "Reset your password."}
+            or copy and paste the link below in your browser: <br> ${process.env.DOMAIN}/${emailType === "VERIFY" ? "verifyemail" : "verifyforgetpasswordtoken"}?token=${hashedToken}
             </p>`
           };
 
