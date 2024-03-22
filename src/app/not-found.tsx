f08c0fe82e708553586a29003ab5cc71f5e783e0
bhanu-sh <bhanu1234sharma@gmail.com>
2024-03-22 19:35:07 +0530

    fix error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   return (
     <div>
         <h2>Not Found</h2>
-        <p>Sorry! We couldn't find the page you're looking for.</p>
+        <p>Sorry! We couldn&apos;t find the page you&apos;re looking for.</p>
         <Link href="/">Return to Home</Link>
     </div>
   )
