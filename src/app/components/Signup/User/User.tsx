2df95876cf342406a4cca4e8c79b27134c9d9689
bhanu-sh <bhanu1234sharma@gmail.com>
2024-03-27 21:48:17 +0530

    seperate signup components

---
+++ src/app/components/Signup/User/User.tsx
@@ -0,0 +1,114 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const UserSignup = () => {
+  const router = useRouter();
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup Success", response.data);
+      toast.success("Signup Success");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.name.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  return (
+    <>
+      <label htmlFor="name">Name</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="name"
+        type="text"
+        value={user.name}
+        placeholder="Name"
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="email"
+        type="email"
+        value={user.email}
+        placeholder="Email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
+        id="password"
+        type="password"
+        value={user.password}
+        placeholder="Password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <span className="text-red-500 pb-2 my-2">
+        {user.password.length > 0 ? (
+          <>
+            {user.password.length < 8
+              ? "Password must be at least 8 characters"
+              : null || user.password.length > 50
+              ? "Password must be at most 50 characters"
+              : null || user.password.includes("password")
+              ? "Password cannot contain the word 'password'"
+              : null || user.password.includes("123")
+              ? "Password cannot contain '123'"
+              : null || user.password.includes(user.name)
+              ? "Password cannot contain your name"
+              : null || user.password.includes(user.email)
+              ? "Password cannot contain your email"
+              : null || user.password.includes("qwerty")
+              ? "Password cannot contain 'qwerty'"
+              : null || user.password.includes("abc")
+              ? "Password cannot contain 'abc'"
+              : null || user.password.includes("123")
+              ? "Password cannot contain '123'"
+              : null || !/[0-9]/.test(user.password)
+              ? "Password must contain at least one number"
+              : null || !/[!@#$%^&*]/.test(user.password)
+              ? "Password must contain at least one special character"
+              : null}
+          </>
+        ) : null}
+      </span>
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 disabled:opacity-50"
+        disabled={buttonDisabled || loading}
+        onClick={onSignup}
+      >
+        Submit
+      </button>
+    </>
+  );
+};
+
+export default UserSignup;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,7 +16,8 @@ export default function SignupPage() {
   const [loading, setLoading] = useState(false);
   const [userType, setUserType] = useState("User");
 
-  const activeStyle = "border-b-4 border-blue-400 mb-5";
+  const userActiveStyle = "border-b-4 border-blue-400";
+  const artistActiveStyle = "border-b-4 border-red-600";
 
   const onSignup = async () => {
     try {
@@ -50,13 +51,21 @@ export default function SignupPage() {
       <h1 className="text-4xl text-center font-bold">{userType + " "}Signup</h1>
       <div className="flex">
         <button
-          className={"mx-auto w-48 px-4" + (userType === "User" ? activeStyle : "")}
+          className={
+            "mx-auto w-48 px-4" +
+            " " +
+            (userType === "User" ? userActiveStyle : "")
+          }
           onClick={() => setUserType("User")}
         >
           User
         </button>
         <button
-          className={"mx-auto w-48 px-4" + (userType === "Artist" ? activeStyle : "")}
+          className={
+            "mx-auto w-48 px-4" +
+            " " +
+            (userType === "Artist" ? artistActiveStyle : "")
+          }
           onClick={() => setUserType("Artist")}
         >
           Artist
@@ -64,6 +73,73 @@ export default function SignupPage() {
       </div>
       <hr />
       {userType === "User" && (
+        <>
+          <label htmlFor="name">Name</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="name"
+            type="text"
+            value={user.name}
+            placeholder="Name"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="email"
+            value={user.email}
+            placeholder="Email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            placeholder="Password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+          <span className="text-red-500 pb-2 my-2">
+            {user.password.length > 0 ? (
+              <>
+                {user.password.length < 8
+                  ? "Password must be at least 8 characters"
+                  : null || user.password.length > 50
+                  ? "Password must be at most 50 characters"
+                  : null || user.password.includes("password")
+                  ? "Password cannot contain the word 'password'"
+                  : null || user.password.includes("123")
+                  ? "Password cannot contain '123'"
+                  : null || user.password.includes(user.name)
+                  ? "Password cannot contain your name"
+                  : null || user.password.includes(user.email)
+                  ? "Password cannot contain your email"
+                  : null || user.password.includes("qwerty")
+                  ? "Password cannot contain 'qwerty'"
+                  : null || user.password.includes("abc")
+                  ? "Password cannot contain 'abc'"
+                  : null || user.password.includes("123")
+                  ? "Password cannot contain '123'"
+                  : null || !/[0-9]/.test(user.password)
+                  ? "Password must contain at least one number"
+                  : null || !/[!@#$%^&*]/.test(user.password)
+                  ? "Password must contain at least one special character"
+                  : null}
+              </>
+            ) : null}
+          </span>
+          <button
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 disabled:opacity-50"
+            disabled={buttonDisabled || loading}
+            onClick={onSignup}
+          >
+            Submit
+          </button>
+        </>
+      )}
+      {userType === "Artist" && (
         <>
           <label htmlFor="name">Name</label>
           <input
